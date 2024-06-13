b559e2216ed0dc07777789a5694d33b6fbe3bc30
Ignacio Ainol <iggnaxios@gmail.com>
2024-06-13 00:19:50 -0400

    Change Slider to react-image-gallery and added fontawesome

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Loading } from './components/Loading';
 import { Footer } from './components/Footer';
 import { ContactScreen } from './screens/ContactScreen';
 import { GalleryScreen } from './screens/GalleryScreen';
-// import { ChangeLanguaje } from './components/ChangeLanguaje';
+import { ChangeLanguaje } from './components/ChangeLanguaje';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -21,59 +21,62 @@ function App() {
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
-        <header>
-          <Container className="mt-4">
-            <img src="" alt="" />
+        <div className="container">
+          <header>
+            <Container className="mt-4">
+              <ChangeLanguaje />
+              <img src="" alt="" />
 
-            <Navbar expand="lg">
-            <Container>
-              <Link to="/">
-                <Navbar.Brand>
-                  <img
-                    src="/assets/images/logo-cobras.png"
-                    height="200"
-                    className="d-inline-block align-top animate__animated animate__tada"
-                    alt="Cobras MC logo"
-                  />
-                </Navbar.Brand>
-              </Link>
+              <Navbar expand="lg">
+                <Container>
+                  <Link to="/">
+                    <Navbar.Brand>
+                      <img
+                        src="/assets/images/logo-cobras.png"
+                        height="200"
+                        className="d-inline-block align-top animate__animated animate__tada"
+                        alt="Cobras MC logo"
+                      />
+                    </Navbar.Brand>
+                  </Link>
 
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto w-100 justify-content-end">
-                  <div className="itemsNavbar">
-                    <Link className="nav-link" to="/">
-                       Inicio
-                    </Link>
-                    <Link className="nav-link" to="/members">
-                      Integrantes
-                      </Link>
-                      <Link className="nav-link" to="/gallery">
-                      Galería de Fotos
-                      </Link>
-                      <Link className="nav-link" to="/contacto">
-                      Contacto
-                    </Link>
-                  </div>
-                </Nav>
-              </Navbar.Collapse>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto w-100 justify-content-end">
+                      <div className="itemsNavbar">
+                        <Link className="nav-link" to="/">
+                          Inicio
+                        </Link>
+                        <Link className="nav-link" to="/members">
+                          Integrantes
+                        </Link>
+                        <Link className="nav-link" to="/gallery">
+                          Galería de Fotos
+                        </Link>
+                        <Link className="nav-link" to="/contacto">
+                          Contacto
+                        </Link>
+                      </div>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
             </Container>
-          </Navbar>
-          </Container>
-        </header>
-        <main>
-        <Container className="mt-3">
-            <Routes>
-              <Route>
-                <Route path="/members" element={<MembersScreen />}></Route>
-                <Route path="/contacto" element={<ContactScreen />}></Route>
-                <Route path="/gallery" element={<GalleryScreen />}></Route>
-                <Route path="/" element={<HomeScreen />}></Route>
-              </Route>
-            </Routes>
-          </Container>
-        </main>
-        <Footer/>
+          </header>
+          <main>
+            <Container className="mt-3">
+              <Routes>
+                <Route>
+                  <Route path="/members" element={<MembersScreen />}></Route>
+                  <Route path="/contacto" element={<ContactScreen />}></Route>
+                  <Route path="/gallery" element={<GalleryScreen />}></Route>
+                  <Route path="/" element={<HomeScreen />}></Route>
+                </Route>
+              </Routes>
+            </Container>
+          </main>
+          <Footer />
+        </div>
       </div>
     </BrowserRouter>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,54 +1,32 @@
-import React from 'react'
-import { Carousel } from 'react-bootstrap'
-import { CarouselImage } from '../components/CarouselImage'
+import React, { Fragment } from 'react';
+import { Carousel } from 'react-bootstrap';
+import { CarouselImage } from '../components/CarouselImage';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 export const HomeScreen = () => {
-  return (
-      <>
-          <Carousel>
-            <Carousel.Item>
-                  <CarouselImage
-                    src="/assets/images/cobras-slider-1.png" 
-                    alt="First slide" 
-                  />
-                <Carousel.Caption>
-                <h3>Cobras MC</h3>
-                <p>To Death</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                  <CarouselImage
-                    src="/assets/images/cobras-slider-2.jpeg" 
-                    alt="First slide" 
-                  />
-                <Carousel.Caption>
-                <h3>Club House</h3>
-                <p>Let's Rock </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                  <CarouselImage
-                    src="/assets/images/cobras-slider-3.png" 
-                    alt="First slide" 
-                  />
-                <Carousel.Caption>
-                <h3>Rebel Rebel</h3>
-                <p>
-                    Heavy Metal is the Law
-                </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-      </Carousel>
-      
+  const images = [
+    {
+      original: './assets/images/cobras-slider-2.jpeg',
+      thumbnail: './assets/images/cobras-slider-2.jpeg',
+    },
+    {
+      original: './assets/images/cobras-slider-1.png',
+      thumbnail: './assets/images/cobras-slider-1.png',
+    },
+    {
+      original: './assets/images/cobras-slider-4.jpeg',
+      thumbnail: './assets/images/cobras-slider-4.jpeg',
+    },
+    {
+      original: './assets/images/cobras-slider-5.jpg',
+      thumbnail: './assets/images/cobras-slider-5.jpg',
+    },
+  ];
 
-      <div className="container mt-5">
-        <div className="row mt-5">
-          <h4>Historia</h4>
-          <p>Hace quince años, en la ciudad de Concepción, un grupo de apasionados por las motocicletas se unió para formar un motoclub que honraría la herencia de la cultura de las dos ruedas. Así nació "Cobras MC", un nombre que resonaría en las carreteras y más allá, como símbolo de camaradería, respeto y pasión por la conducción.</p>
-          <p>Desde sus comienzos, los miembros de Cobras MC se comprometieron a establecer un estándar excepcional en el mundo de los motoclubs. Respetando rigurosamente todas las reglas de seguridad y ética, el club se convirtió en un faro de integridad y responsabilidad en la comunidad motorizada de la región.</p>
-          <p>A lo largo de los años, Cobras MC ha forjado lazos indestructibles entre sus miembros, cimentados en el amor compartido por la libertad en dos ruedas. Juntos, han explorado intrépidamente las carreteras sinuosas de Chile, descubriendo nuevos horizontes y creando recuerdos inolvidables en cada curva.</p>
-        </div>
-      </div>
-      </>
-  )
-}
+  return (
+    <Fragment>
+      <ImageGallery items={images} />
+    </Fragment>
+  );
+};

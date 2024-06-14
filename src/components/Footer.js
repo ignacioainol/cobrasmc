14c3017d8dd84faeb4bb47b80e87544c2caa782f
Ignacio Ainol <iggnaxios@gmail.com>
2024-06-13 20:32:44 -0400

    adding images and changing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,14 @@ export const Footer = () => {
         <Row>
           <Col md={4}>
             <h5>Información de contacto</h5>
-            <p>Dirección:Juan Martínez de Rozas 943</p>
+            <p>
+              <i className="fa-solid fa-location-dot"></i> Juan Martínez de
+              Rozas 943
+            </p>
             {/* <p>Teléfono: 555-123-456</p> */}
-            <p>Email: cobrasmc@gmail.com</p>
+            <p>
+              <i className="fa-solid fa-envelope"></i> cobrasmc@gmail.com
+            </p>
           </Col>
           <Col md={4}>
             <h5>Síguenos en redes sociales</h5>

926286e2ee78e5f63ce95a5b740a33b59c1c8d1b
Ignacio Ainol <iggnaxios@gmail.com>
2024-08-05 21:29:17 -0400

    responsive and added post test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
 export const HomeScreen = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const loadPosts = async () => {
+      const response = await fetch('http://localhost:8000/wp-json/wp/v2/posts');
+      if (!response.ok) {
+        // oups! something went wrong
+        return;
+      }
+
+      const posts = await response.json();
+      setPosts(posts);
+    };
+
+    loadPosts();
+  }, []);
   const images = [
     {
       original: './assets/images/slider/cobras-slider-2.jpeg',
@@ -25,6 +40,9 @@ export const HomeScreen = () => {
   return (
     <Fragment>
       <ImageGallery autoPlay={true} items={images} slideInterval={6000} />
+      {posts.map((post, index) => (
+        <p>{post.title.rendered}</p>
+      ))}
     </Fragment>
   );
 };
